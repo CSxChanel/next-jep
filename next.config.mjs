f7bb65ca4b0613e583cad887afe9abda09df07f1
Cp_Sudrajat <120399183+CSxChanel@users.noreply.github.com>
2025-02-24 17:38:23 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+import withPWA from "next-pwa";
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true
+  }
+});
+
 export default nextConfig;
