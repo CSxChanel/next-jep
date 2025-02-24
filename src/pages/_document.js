7ae8e4a745e3cf5fb733582df1e68dcc64dfc054
Cp_Sudrajat <120399183+CSxChanel@users.noreply.github.com>
2025-02-24 17:40:16 +0700

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="id-ID">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#007bff" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </Head>
       <body >
         <Main />
         <NextScript />
