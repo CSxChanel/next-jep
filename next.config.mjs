5e137abacc3eb2f84cff3f8fe38b5bdd7b7812f4
Cp_Sudrajat <120399183+CSxChanel@users.noreply.github.com>
2025-03-02 17:52:55 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
 import withPWA from "next-pwa";
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Tetap di luar withPWA
+  reactStrictMode: true,
 };
 
 export default withPWA({
   ...nextConfig,
   pwa: {
     dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development", // Disable saat dev
+    mode: "production",
+    runtimeCaching: [],
   },
 });
